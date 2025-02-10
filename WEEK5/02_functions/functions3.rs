609a1750d786eed0e597da6489e8828f6210a500
AbdullaAlMehrzi <a3ralm12@gmail.com>
2025-02-10 10:06:47 +0400

    Add files via upload

---
+++ WEEK5/02_functions/functions3.rs
@@ -0,0 +1,11 @@
+// TODO: Add the missing type of the argument `num` after the colon `:`.
+fn call_me(num: u8) {
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
+
+fn main() {
+    // TODO: Fix the function call.
+    call_me(3);
+}
