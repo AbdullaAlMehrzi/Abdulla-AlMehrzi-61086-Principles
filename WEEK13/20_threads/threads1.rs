95482a000ed44d8fd2f5705f53b01b49038ac9e5
AbdullaAlMehrzi <a3ralm12@gmail.com>
2025-04-15 15:32:02 +0400

    Add files via upload

---
+++ WEEK13/20_threads/threads1.rs
@@ -0,0 +1,33 @@
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
+
+fn main() {
+    let mut handles = Vec::new();
+    for i in 0..10 {
+        let handle = thread::spawn(move || {
+            let start = Instant::now();
+            thread::sleep(Duration::from_millis(250));
+            println!("Thread {i} done");
+            start.elapsed().as_millis()
+        });
+        handles.push(handle);
+    }
+
+    let mut results = Vec::new();
+    for handle in handles {
+        // TODO: Collect the results of all threads into the `results` vector.
+        // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        results.push(handle.join().unwrap());
+    }
+
+    if results.len() != 10 {
+        panic!("Oh no! Some thread isn't done yet!");
+    }
+
+    println!();
+    for (i, result) in results.into_iter().enumerate() {
+        println!("Thread {i} took {result}ms");
+    }
+}
